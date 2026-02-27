0ace85fa3dc138218f415838c0eaa20539fbc197
Your Name <you@example.com>
2026-02-27 08:05:29 +0530

    Create admin user on deploy

--- build.sh
+++ build.sh
@@ -2,3 +2,14 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin123"
+    )
+EOF
